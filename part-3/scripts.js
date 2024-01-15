2e23b1cff0c99ee4fadd4dbcf54509a3e536c446
Tiago Lagreca Peixoto <61335951+tiagolagrecap@users.noreply.github.com>
2024-01-14 21:51:25 -0800

    Add files via upload

---
+++ part-3/scripts.js
@@ -0,0 +1,79 @@
+/* 
+const output = document.querySelector("#output");
+const button = document.querySelector("#set-alarm");
+
+
+
+function setAlarm() {
+  setTimeout(() => {
+    output.textContent = "Wake up!";
+  }, 1000);
+}
+
+button.addEventListener("click", setAlarm);
+
+
+
+
+function alarm(person, delay) {
+    return new Promise((resolve, reject) => {
+        if (delay < 0){
+            throw new Error("Alarm delay must not be negative");
+        }
+        setTimeout(()=> {
+            resolve(`Wake up, ${person}!`)
+        }, delay)
+
+    })
+}
+
+
+const name = document.querySelector("#name");
+const delay = document.querySelector("#delay");
+const button = document.querySelector("#set-alarm");
+const output = document.querySelector("#output");
+
+
+function alarm(person, delay){
+    return new Promise((resolve, reject) =>{
+        if(delay < 0){
+            throw new Error("Alarm delay must not be negative!!!")
+        }
+        setTimeout(() =>{
+            resolve(`Wake up, ${person}!`);
+        }, delay);
+    })
+}
+
+button.addEventListener("click", () => {
+    alarm(name.value, delay.value)
+      .then((message) => (output.textContent = message))
+      .catch((error) => (output.textContent = `Couldn't set alarm: ${error}`));
+  });
+ */
+
+
+  const name = document.querySelector("#name");
+const delay = document.querySelector("#delay");
+const button = document.querySelector("#set-alarm");
+const output = document.querySelector("#output");
+
+function alarm(person, delay) {
+  return new Promise((resolve, reject) => {
+    if (delay < 0) {
+      throw new Error("Alarm delay must not be negative");
+    }
+    setTimeout(() => {
+      resolve(`Wake up, ${person}!`);
+    }, delay);
+  });
+}
+
+button.addEventListener("click", async () => {
+  try {
+    const message = await alarm(name.value, delay.value);
+    output.textContent = message;
+  } catch (error) {
+    output.textContent = `Couldn't set alarm: ${error}`;
+  }
+});
